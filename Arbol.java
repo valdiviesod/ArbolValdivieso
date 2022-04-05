0a5d24a1f64cbe33372824e9faaca5a2f070c94d
DANIEL VALDIVIESO <dzvaldiviesod@gmail.com>
2022-04-04 19:07:53 -0700

    remove added

--- Arbol.java
+++ Arbol.java
@@ -93,4 +93,80 @@ public class Arbol {
     }
 
 
+    public boolean remove(int i) {
+        Nodo aux = root;
+        Nodo father = root;
+        boolean isLeft = true;
+        while (aux.index != i) {
+            father = aux;
+            if (i < aux.index) {
+                isLeft = true;
+                aux = aux.left;
+            } else {
+                isLeft = false;
+                aux = aux.left;
+            }
+            if (aux == null) {
+                return false;
+            }
+        }
+        if (aux.left == null && aux.right == null) {
+            if (aux == root) {
+                root = null;
+            } else if (isLeft) {
+                father.left = null;
+            } else {
+                father.right = null;
+            }
+        } else if (aux.right == null) {
+            if (aux == root) {
+                root = aux.left;
+            } else if (isLeft) {
+                father.left = aux.left;
+            } else {
+                father.right = aux.left;
+            }
+        } else if (aux.left == null){
+            if (aux == root) {
+                root = aux.right;
+            } else if (isLeft) {
+                father.left = aux.right;
+            } else {
+                father.right = aux.left;
+            }
+        }else {
+            Nodo replacement = getNodeReplacement(aux);
+            if (aux == root){
+                root = replacement;
+            }
+            else if (isLeft){
+                father.left = replacement;
+            }
+            else {
+                father.right = replacement;
+            }
+            replacement.left = aux.left;
+        }
+        return true;
+    }
+
+    public Nodo getNodeReplacement(Nodo NodeReplacement){
+        Nodo replacementFather = NodeReplacement;
+        Nodo replacement = NodeReplacement;
+        Nodo aux = NodeReplacement.right;
+        while (aux != null){
+            replacementFather = replacement;
+            replacement = aux;
+            aux = aux.left;
+        }
+        if (replacement != NodeReplacement.right){
+            replacementFather.left = replacement.right;
+            replacement.right = NodeReplacement.right;
+        }
+        return  replacement;
+    }
+}
+
+
+
 }
